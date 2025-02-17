929d17dac1b72cbe1c18594c64af9c4c3b34d463
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-02-17 23:06:38 +0300

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269858.py
@@ -0,0 +1,22 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+password = input()
+found = False
+n = int(input())
+barks = []
+for _ in range(n):
+    barks.append(input())
+
+
+for i in range(len(barks)):
+    for j in range(i,len(barks)):
+        if password in barks[i]+barks[j] or password in barks[j]+barks[i]:
+            print("YES")
+            found = True
+            break
+    if found:
+        break
+if not found:
+    print("NO")
+
+    
