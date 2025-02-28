76ad7edae620580da07f73ab9498c9f0d6f89777
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-02-28 12:10:13 -0800

    Created 'Watermelon' by A2SV Hub

---
+++ 28-Feb-2025/Watermelon 287356.py
@@ -0,0 +1,9 @@
+# Problem: Watermelon - https://codeforces.com/problemset/problem/4/A
+
+w = int(input())  # Input the weight of the watermelon
+
+# Check if the weight is even and at least 4
+if w % 2 == 0 and w >= 4:
+    print("YES")
+else:
+    print("NO")
