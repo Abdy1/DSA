94bcc116db4c6cae24a338014fadf659ef3745d3
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-03-12 12:18:10 -0700

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 12-Mar-2025/A. Beautiful Matrix 304381.py
@@ -0,0 +1,13 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = []
+coordinate = []
+for i in range(5):
+    columns = list(map(int,input().split()))
+    matrix.append(columns)
+    if 1 in columns:
+        coordinate.append(i)
+        coordinate.append(columns.index(1))
+
+print(abs(2-coordinate[0]) + abs(2-coordinate[1]))
+
