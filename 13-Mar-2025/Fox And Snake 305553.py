b3e11bd9888fc34cb16aa63b7fdd810415d77c79
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-03-13 10:40:50 -0700

    Created 'Fox And Snake' by A2SV Hub

---
+++ 13-Mar-2025/Fox And Snake 305553.py
@@ -0,0 +1,17 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n, m = map(int,input().split())
+
+forward = False
+
+for i in range(n):
+    if i % 2 == 0:
+        print("".join(['#']  * m))
+    else:
+        times = m - 1
+        if forward:
+            print("#"+"".join(['.'] * times))
+        elif not forward:
+            print("".join(['.'] * times)+"#")
+        forward = not forward
+
