a64a820598a0fa7cb27b1b79b4a06e0272dbae41
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-04-22 21:41:23 +0300

    Created 'Third Maximum Number' by A2SV Hub

---
+++ 22-Apr-2025/Third Maximum Number 338881.py
@@ -0,0 +1,20 @@
+# Problem: Third Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        count = 0
+        visit = set()
+        max_number = 0
+
+        for n in nums:
+            if n not in visit:
+                count += 1
+                visit.add(n)
+
+            if count == 3:
+                return n
+            
+            max_number = max(max_number, n)
+            
+        return max_number
