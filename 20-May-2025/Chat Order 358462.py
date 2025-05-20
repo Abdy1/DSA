a431f326862ec3b18dfec149cb30320f69ab2cde
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-05-20 22:50:55 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 20-May-2025/Chat Order 358462.py
@@ -0,0 +1,16 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+from collections import deque
+def work(messages):
+    seen = set()
+    listt = deque()
+    
+    for name in reversed(messages):
+        if name not in seen: 
+            seen.add(name)
+            listt.appendleft(name)
+    return listt
+t = int(input())
+messages = [input().strip() for _ in range(t)]
+result = work(messages)
+print('\n'.join(reversed(result)))
