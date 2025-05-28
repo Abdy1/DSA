1e2eb47864568fbf322c4ea32993e0d99166f799
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-05-28 21:46:09 +0300

    Created 'Books' by A2SV Hub

---
+++ 28-May-2025/Books 360552.py
@@ -0,0 +1,18 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+a = list(map(int,input().split()))
+
+slow = 0
+run = 0
+max_book = 0
+for fast in range(len(a)):
+    run += a[fast]
+    while run > t:
+        run -= a[slow]
+        slow += 1
+    max_book = max(max_book, fast - slow + 1)
+
+print(max_book)
+        
+ 
