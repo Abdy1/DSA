388108bf574889dac79a3edabb0ff407b6c9f903
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2025-06-23 22:42:45 +0300

    Created 'C - Get in the Center, You Weirdos!' by A2SV Hub

---
+++ A2SV Contest #19 23-Jun-2025/C - Get in the Center, You Weirdos! 369640.py
@@ -0,0 +1,16 @@
+# Problem: C - Get in the Center, You Weirdos! - https://codeforces.com/gym/617023/problem/C
+
+def min_moves_to_center(test_cases):
+    results = []
+    for n in test_cases:
+        k = n // 2
+        total_moves = (4 * k * (k + 1) * (2 * k + 1)) // 3
+        results.append(total_moves)
+    return results
+
+
+t = int(input())
+test_cases = [int(input()) for _ in range(t)]
+answers = min_moves_to_center(test_cases)
+for ans in answers:
+    print(ans)
